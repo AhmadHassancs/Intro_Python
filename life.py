8ae63737878f76958edba97d2037393583adb908
ahmadhassancs <hassan42082@gmail.com>
2021-09-09 08:47:20 +0500

    1st commit

---
+++ life.py
@@ -0,0 +1,8 @@
+import math
+theAnswer = 42
+
+def quote_marvin():
+        print('Life is like a river, It just keeps going on no matter what happens.')
+        
+def square_root(x):
+    return math.sqrt(x)
---
+++ python practice.py
@@ -0,0 +1,8 @@
+# CONTINUE prectice in loop
+for i in range(2,40):
+    if i%2==0:
+#        print('woooaa',i)
+        continue
+    else:
+        print(i)
+print('the end')

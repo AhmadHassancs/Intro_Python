cae978a7026cc959b6c2564cdc64059abdf4acb4
ahmadhassancs <hassan42082@gmail.com>
2022-03-11 09:02:38 +0500

    just

---
+++ control_statements.py
@@ -0,0 +1,43 @@
+# age = int(input('Please enter your age: '))
+
+# if age > 15:
+#     print("Congrates, you are an Adult")
+# else:
+#     print('Not Adult')
+
+###############################################################
+
+# light = input('Please input name of color of the light: ')
+# light = light.lower()
+# if light == 'green':
+#     print('you can go')
+# elif light == 'yellow':
+#     print('Get Ready')
+# else:
+#     print('Stop')
+
+################################################################
+# b = input('Please Enter your Name: ')
+# a = int(input('Please Enter Your Age: '))
+
+# if a < 15:
+#     print(f'Dear {b} You are not allowed to drive becouse you are too young')
+# elif a >= 15 and a < 55:
+#     print(f'Dear {b} You can drive')
+# else:
+#     print(f'Dear {b} You are too old to drive')
+
+#################################################################
+
+# num = int(input('Please Enter a Number: '))
+
+# if num >=0:
+#     print("Positive")
+#     if num%2 == 0:
+#         print('Even')
+#     else:
+#         print('Odd')
+# else:
+#     print('Negative')
+
+##################################################################
---
+++ loop.py
@@ -0,0 +1,64 @@
+# x = 0
+
+# while x < 5:
+#     print('X is: ',x)
+#     x += 1
+
+#########################################################
+
+# x = 0
+
+# while x < 5:
+#     print('X is: ',x)
+#     x += 1
+# else:
+#     print('Loop completed!')
+
+#########################################################
+
+# l = [0,10,11,12,13,14]
+# t = (0,10,11,12,13,14)
+# s = {0,10,11,12,13,1}
+
+# for i in l:
+#     print('These are comming from list: ',i)
+
+# for i in t:
+#     print('These are comming from Tuple: ',i)
+
+# for i in s:
+#     print('These are comming from a Set: ',i)
+
+##########################################################
+
+# name = input('Please Enter your Name: ')
+# print(f'The number of characters in your name are {len(name)}')
+# for i in name:
+#     print(i)
+
+###########################################################
+
+# s = 'this is a string'
+# print(s[::-1])
+
+##########################################################
+
+# a = ['this', 'crazzy', 'list']
+
+# for i in a:
+#     if i == 'crazzy':
+#         break
+
+#     print(i)
+
+##########################################################
+
+# a = ['this', 'crazzy', 'list', 'is', 'a', 'little', 'longer']
+
+# for i in a:
+#     if i == 'is':
+#         continue
+#     print(i)
+
+###########################################################
+

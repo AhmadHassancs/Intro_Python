d7b1d82a8cc8b9dbde344f2b01bf3858baa3970a
ahmadhassancs <hassan42082@gmail.com>
2022-03-12 21:54:16 +0500

    just another

---
+++ function.py
@@ -0,0 +1,71 @@
+# x = int(input("Enter 1st number: "))
+# y = int(input("Enter 2nd number: "))
+
+# def my_fun(a,b):
+#     print('The sum is: ',a+y)
+
+# my_fun(x,y)
+
+########################################################
+
+# x = int(input("Enter 1st number: "))
+# y = int(input("Enter 2nd number: "))
+
+# def my_fun(a,b):
+#     print('The Sum is: ',a+y)
+#     print('The Difference is: ',a-y)
+#     print('The Multiplication is: ',a*y)
+#     print('The Division is: ',a/y)
+
+# my_fun(x,y)
+
+########################################################
+
+# x = int(input("Enter 1st number: "))
+# y = int(input("Enter 2nd number: "))
+
+# def my_fun(first_number,second_number):
+#     print('The Sum is: ',first_number+second_number)
+#     print('The Difference is: ',first_number-second_number)
+#     print('The Multiplication is: ',first_number*second_number)
+#     print('The Division is: ',first_number/second_number)
+
+# my_fun(first_number = x,second_number = y)
+# my_fun(first_number = y,second_number =x)
+# my_fun(second_number = x,first_number = y)
+
+#############################################################
+
+# x = float(input('Please Enter Radius: '))
+# def area_circle(radius, pi = 3.14):
+#     radius = radius*radius
+#     area = radius*pi
+#     print('The Area of the Circle is: ',area)
+
+# area_circle(radius=x) 
+
+#############################################################
+
+# def another(name, address, *marks):
+#     print('My name is: ',name)
+#     print('My address is: ',address)
+#     # print('I have obtained: ',marks)
+#     for i in marks:
+#         print('Marks: ',i)
+
+# another('hassan', 'mardan',10,10,20,30)
+
+#############################################################
+
+
+# def another(name, address, **marks):
+#     print('My name is: ',name)
+#     print('My address is: ',address)
+#     # print('I have obtained: ',marks)
+#     for i,j in marks.items():
+#         print(f'You got {j} Marks in {i} Subject ')
+
+# another(name='hassan', address='mardan', elg = 10,math = 10,history = 20,science = 30)
+
+##############################################################
+
